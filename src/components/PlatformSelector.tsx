6dfea447c767af09c7af294af52c340baf32ad5b
Vishrut kevadiya <vishrutk@my.yorku.ca>
2023-10-20 22:39:17 -0600

    Refactor: fetching and storing platform onto the cache and only fetch from the back-end after 24hr

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -18,7 +18,7 @@ const PlatformSelector = ({ onSelectedPlatform, selectedPlatform }: Props) => {
         <Menu>
             <MenuButton as={Button} rightIcon={<BsChevronDown />}> {selectedPlatform?.name || 'Platform'}</MenuButton>
             <MenuList>
-                {data.map(platform => <MenuItem onClick={() => onSelectedPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>)}
+                {data?.results.map(platform => <MenuItem onClick={() => onSelectedPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>)}
             </MenuList>
         </Menu>
     )
