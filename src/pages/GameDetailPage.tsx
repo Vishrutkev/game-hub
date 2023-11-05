2f4a8c17fb38d62e92e95a14517d83f8261a5268
Vishrut kevadiya <vishrutk@my.yorku.ca>
2023-11-05 16:32:43 -0700

    Changed GameTRailer.tsx to GameTrailer.tsx

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -4,7 +4,7 @@ import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameScreenShots from "../components/GameScreenShots";
-import GameTrailer from "../components/GameTRailer";
+import GameTrailer from "../components/GameTrailer";
 
 
 const GameDetailPage = () => {
